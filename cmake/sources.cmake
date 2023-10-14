fcc1c90294e7ea00f87207ed7a2cf4f5884e4f69
m <a@b.c>
2023-10-14 22:36:52 +0200

    move include files into separate folder and setup cmake

---
+++ cmake/sources.cmake
@@ -0,0 +1,17 @@
+
+set(ALGLIB_SOURCES src/alglibinternal.cpp
+                   src/alglibmisc.cpp
+                   src/ap.cpp
+                   src/dataanalysis.cpp
+                   src/diffequations.cpp
+                   src/fasttransforms.cpp
+                   src/integration.cpp
+                   src/interpolation.cpp
+                   src/kernels_avx2.cpp
+                   src/kernels_fma.cpp
+                   src/linalg.cpp
+                   src/optimization.cpp
+                   src/solvers.cpp
+                   src/specialfunctions.cpp
+                   src/statistics.cpp)
+
